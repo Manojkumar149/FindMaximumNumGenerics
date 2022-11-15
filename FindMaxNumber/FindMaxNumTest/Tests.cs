c82d38fede3aecfd384e33a23c6989e695248187
Manoj Kumar Manukonda <manojkumarmanukonda77@gmail.com>
2022-11-15 09:02:30 +0530

    [Manoj] Add: FindMaxInteger

---
+++ FindMaxNumber/FindMaxNumTest/Tests.cs
@@ -0,0 +1,34 @@
+﻿using FindMaxNumber;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FindMaxNumTest
+{
+    public class Tests
+    {
+        FindMaxNum maxNumber = new FindMaxNum();
+        [Test]
+        public void GivenMaxFirstNumInteger_WhenAnalysed_ShouldReturnFirstMax()
+        {
+            int result = maxNumber.FindMaxInteger(15, 5, 10);
+            Assert.AreEqual(15, result);
+        }
+        [Test]
+        public void GivenMaxSecondNumInteger_WhenAnalysed_ShouldReturnSecondMax()
+        {
+            int result = maxNumber.FindMaxInteger(5, 15, 10);
+            Assert.AreEqual(15, result);
+        }
+        [Test]
+        public void GivenMaxThirdNumInteger_WhenAnalysed_ShouldReturnThirdMax()
+        {
+            int result = maxNumber.FindMaxInteger(10, 5, 15);
+            Assert.AreEqual(15, result);
+        }
+    }
+}
---
+++ FindMaxNumber/FindMaxNumber/FindMaxNum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxNumber
+{
+    public class FindMaxNum
+    {
+        public int FindMaxInteger(int a, int b, int c)
+        {
+            if (a > b && a > c)
+                return a;
+            if (b > a && b > c)
+                return b;
+            else
+                return c;
+        }
+    }
+}
